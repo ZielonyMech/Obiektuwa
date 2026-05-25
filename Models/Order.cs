08f64c69e3e5ab73cdb83b786b97f6a9ff16a1c2
Lukasz Niebrzydowski <lukasz.niebrzydowski.113367@student.pb.edu.pl>
2026-05-25 21:59:20 +0200

    Dodanie klasy Order

---
+++ Models/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obiektuwa.Models
+{
+    public class Order
+    {
+        public enum OrderState{
+            inProgress,
+            finished,
+            canceled
+        }
+
+
+        public int id;
+        public string comment { get; init; }
+        public bool isTakeaway { get; set; }
+        private OrderState state { get; set; }
+
+        public List<Product> positions { get; set; } = new List<Product>();
+
+
+        public void ChangeState() {
+            if (state == OrderState.inProgress) {
+                state = OrderState.finished;
+            }
+        }
+
+        public int CalculateFinalPrice() {
+            if (positions == null) {
+                return 0; 
+            }
+            return (int)positions.Sum(p => p.productPrice);
+        }
+  
+
+    
+
+        
+    }
+}
